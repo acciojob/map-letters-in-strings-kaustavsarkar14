398f0e3326cc208025382f2de2c09ef070a8a25d
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-17 11:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const s = prompt()
+function mapLetters(s) {
+	const letterMap = {}
+	for(let i=0; i<s.length; i++){
+	  if(letterMap[s[i]]==undefined)
+	    letterMap[s[i]] = [i]
+	  else
+	    letterMap[s[i]].push(i)
+	}
+	console.log(letterMap)
+	alert(letterMap)
+}
+mapLetters(s)
